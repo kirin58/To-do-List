41152a4ac0c8a6b99a6e7fb3ec969b1eac1384d9
PT <peepeerapat3105@gmail.com>
2025-07-15 13:54:09 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <p className="text-4xl text-red-900 ">
         Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
